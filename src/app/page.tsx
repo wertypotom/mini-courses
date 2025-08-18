57deb6a7e3a71be71aaa4d5c8ab5d1b7bc1dfada
Andrey Povstyanko <andrey.povstyanko@m-files.com>
2025-08-17 19:49:45 -0500

    testing ssh push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
       <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
         <div className='flex gap-4 items-center flex-col sm:flex-row'>
-          <Button>Outline</Button>
+          <Button>Testing SSH</Button>
         </div>
       </main>
     </div>
