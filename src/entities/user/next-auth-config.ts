8b49d1c367e185e1750a8e00931cf2b438d2e30c
Andrey Povstyanko <andrey.povstyanko@m-files.com>
2025-09-16 21:37:48 -0500

    fixed types

--- src/entities/user/next-auth-config.ts
+++ src/entities/user/next-auth-config.ts
@@ -4,9 +4,10 @@ import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { compact } from "lodash-es";
 import { privateConfig } from "@/shared/config/private";
+import { Provider } from "next-auth/providers/index";
 
 export const nextAuthConfig: AuthOptions = {
-  adapter: PrismaAdapter(dbClient),
+  adapter: PrismaAdapter(dbClient) as Provider["options"],
   providers: compact([
     privateConfig.GITHUB_ID &&
       privateConfig.GITHUB_SECRET &&
